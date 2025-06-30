0df214fae502f78b03b0887b13d12f521f0178f1
SuelanyBrito <suelanybr@gmail.com>
2025-06-30 13:35:38 -0300

    mudança de textos

--- src/app/pages/about/thanks.component.ts
+++ src/app/pages/about/thanks.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {MatFabButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-about',
   templateUrl: './thanks.component.html',
+  imports: [
+    RouterLink,
+    MatFabButton
+  ],
   styleUrls: ['./thanks.component.scss']
 })
 export class ThanksComponent implements OnInit {

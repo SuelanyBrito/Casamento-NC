ab68aab409abe743bfe6fc243687cb65ae7907dd
SuelanyBrito <suelanybr@gmail.com>
2025-07-01 15:14:45 -0300

    finish carrousel

--- src/app/pages/casal/casal.component.ts
+++ src/app/pages/casal/casal.component.ts
@@ -1,18 +1,14 @@
-import { Component } from '@angular/core';
-import {NavbarComponent} from "../../sharepage/navbar/navbar.component";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NavbarComponent } from "../../sharepage/navbar/navbar.component";
 
 @Component({
   selector: 'app-casal',
   templateUrl: './casal.component.html',
-  imports: [
-    NavbarComponent
-  ],
-  styleUrls: ['./casal.component.scss']
+  styleUrls: ['./casal.component.scss'],
+  standalone: true,
+  imports: [NavbarComponent]
 })
-export class CasalComponent {
-
-  constructor() { }
-
+export class CasalComponent implements OnInit, OnDestroy {
   carouselImages = [
     'assets/img/carousel/70.cmyk.webp',
     'assets/img/carousel/71.cmyk.webp',
@@ -22,7 +18,25 @@ export class CasalComponent {
     'assets/img/carousel/3.cmyk.webp',
     'assets/img/carousel/4.cmyk.webp'
   ];
+
   currentImage = 0;
+  private intervalId: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // Começa a trocar automaticamente a cada 4 segundos
+    this.intervalId = setInterval(() => {
+      this.nextImage();
+    }, 4000);
+  }
+
+  ngOnDestroy(): void {
+    // Limpa o intervalo quando o componente for destruído
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 
   nextImage() {
     this.currentImage = (this.currentImage + 1) % this.carouselImages.length;
@@ -31,5 +45,4 @@ export class CasalComponent {
   prevImage() {
     this.currentImage = (this.currentImage - 1 + this.carouselImages.length) % this.carouselImages.length;
   }
-
 }
